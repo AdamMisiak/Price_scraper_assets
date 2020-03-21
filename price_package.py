b49b20be1c74e24fed174d5d55a4064887578578
AdamMisiak <adammisiak3@gmail.com>
2020-03-21 10:55:02 +0100

    third commit

--- price_package.py
+++ price_package.py
@@ -14,8 +14,6 @@ url_usd = 'https://transferwise.com/pl/currency-converter/usd-to-pln-rate'
 
 headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.117 Safari/537.36'}
 
-# ZROB ASYNCHORNICZNIE REQUEST (HTTPX)!!! + ROZBIJ NA MODULY?? + JQUERY PRZESYLANIE JSONA do price scraper
-# OGARNIJ SCARLETTE!!
 
 async def get_url(url):
     # page = requests.get(url, headers = headers)
